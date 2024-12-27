c069134ddbedc4fe478c3ce77dbf0f5f8db8bab5
Im-KVA <130117095+Im-KVA@users.noreply.github.com>
2024-12-28 01:23:13 +0700

    Update firebase.js

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD4FjuFaCoJD3ogJJnLaFNxbIDnGsZ7fyY",
+  apiKey: "yourAPIkey",
   authDomain: "mangaseeking-website.firebaseapp.com",
   projectId: "mangaseeking-website",
   storageBucket: "mangaseeking-website.firebasestorage.app",
   messagingSenderId: "205994896305",
-  appId: "1:205994896305:web:878af188523172e011eb90",
-  measurementId: "G-84ZJWC4FYZ",
+  appId: "yourAppId",
+  measurementId: "...",
 };
 
 // Initialize Firebase
